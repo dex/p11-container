a371a8cb13c9dc2fa464aaa7e5fb5ee8036bcc59
Dex Chen <chienhua@gmail.com>
2024-09-12 21:11:46 +0800

    update ubuntu container

--- run.sh
+++ run.sh
@@ -9,12 +9,11 @@ export TPM2_PKCS11_STORE
 if [ -z "$(docker images -q ${CONTAINER})" ]; then
     docker build -t ${CONTAINER} - <<EOF
 FROM ubuntu:24.04
-RUN apt update && apt install -y p11-kit libtpm2-pkcs11-1 libengine-pkcs11-openssl openssl opensc
+RUN apt update && apt install -y p11-kit libengine-pkcs11-openssl openssl gnutls-bin
 RUN mkdir -p /etc/pkcs11/modules && echo "module: /usr/lib/x86_64-linux-gnu/pkcs11/p11-kit-client.so" | tee /etc/pkcs11/modules/p11-kit-client.module
-RUN echo "alias p11cmd='pkcs11-tool --module /usr/lib/x86_64-linux-gnu/pkcs11/p11-kit-client.so'" | tee /root/.bash_aliases
 EOF
 fi
 
-eval "$(p11-kit server --provider "${PROVIDER}" "pkcs11:")"
+eval "$(p11-kit server ${PROVIDER:+ --provider ${PROVIDER}} 'pkcs11:')"
 docker run --rm -it -v "${P11_KIT_SERVER_ADDRESS#*=}":"${P11_KIT_SERVER_ADDRESS#*=}" -e P11_KIT_SERVER_ADDRESS="${P11_KIT_SERVER_ADDRESS}" ${CONTAINER}
 kill -15 "${P11_KIT_SERVER_PID}"
