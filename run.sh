dff8595a4d50b78199979e77194b599b80a1222c
Dex Chen <chienhua@gmail.com>
2024-09-12 21:27:16 +0800

    update Dockerfile

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ if [ -z "$(docker images -q ${CONTAINER})" ]; then
     docker build -t ${CONTAINER} - <<EOF
 FROM ubuntu:24.04
 RUN apt update && apt install -y p11-kit libengine-pkcs11-openssl openssl gnutls-bin
-RUN mkdir -p /etc/pkcs11/modules && echo "module: /usr/lib/x86_64-linux-gnu/pkcs11/p11-kit-client.so" | tee /etc/pkcs11/modules/p11-kit-client.module
+RUN mkdir -p /etc/pkcs11/modules && echo "module: /usr/lib/x86_64-linux-gnu/pkcs11/p11-kit-client.so" > /etc/pkcs11/modules/p11-kit-client.module
 EOF
 fi
 
